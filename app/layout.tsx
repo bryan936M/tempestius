4c81c86a784d5a28397b3d9e543f51e51164963c
bryan936M <bryanmurasira@gmail.com>
2024-11-30 12:05:57 +0200

    ch/Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
